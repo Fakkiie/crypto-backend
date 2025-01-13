595b60a6c775d1df52c66f419200397e47c7ad0f
Ethan Konkolowicz <ethan.konkolowicz@gmail.com>
2025-01-13 14:39:54 -0500

    added base route for TESTING

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ use get_token_price::get_token_price;
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/token_price/:id", get(get_token_price));
+        .route("/token_price/:id", get(get_token_price))
+        .route("/", get(|| async { "api test" }));
     
     // run server on port 8000
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
