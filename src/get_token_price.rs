506080d49680c0e07df053fc1a9b49b701708296
Ethan Konkolowicz <ethan.konkolowicz@gmail.com>
2025-01-13 14:28:08 -0500

    file structure changes, APIafied it;

---
+++ src/get_token_price.rs
@@ -0,0 +1,36 @@
+use reqwest; 
+use serde_json::Value; 
+use axum::{
+    routing::{get, post},
+    Router,
+    Json, extract::Path,
+};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+
+#[derive(Serialize)]
+pub struct TokenPrice {
+    id: String,
+    price: String,
+}
+
+pub async fn get_token_price(Path(id): Path<String>) -> Json<TokenPrice> {
+   let url = format!("https://api.jup.ag/price/v2?ids={}", id);
+
+   // Fetch the token price asynchronously.
+   let response = reqwest::get(&url).await.expect("Failed to fetch data from API");
+   let json: Value = response.json().await.expect("Failed to parse JSON");
+
+   let data = json["data"].as_object().expect("Missing data field");
+   let price = if let Some(token_data) = data.get(&id) {
+       token_data["price"].as_str().unwrap_or("N/A").to_string()
+   } else {
+       "No data found".to_string()
+   };
+
+   // Return the token price.
+   Json(TokenPrice {
+       id,
+       price,
+   })
+}
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use reqwest; 
+mod get_token_price;
+use axum::{
+    routing::{get, post},
+    Router,
+    Json, extract::Path,
+};
+use std::net::SocketAddr;
+use get_token_price::get_token_price;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/token_price/:id", get(get_token_price));
+    
+    // run server on port 8000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    println!("Server running at http://{addr}");
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+}
