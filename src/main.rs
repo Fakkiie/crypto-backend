e5432b39b4b341d3c65b141f9f314bd35a1bbc8e
Ethan Konkolowicz <ethan.konkolowicz@gmail.com>
2025-01-14 10:07:10 -0500

    fixed cors issues

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,22 @@ use axum::{
     Router,
 };
 use std::net::SocketAddr;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
     let app = Router::new()
         .route("/token_price/:id", get(get_token_price))
         .route("/token_balance", post(get_token_balance))
-        .route("/", get(|| async { "api test" }));
+        .route("/", get(|| async { "api test" }))
+        .layer(cors);
 
     // run server on port 8000
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
